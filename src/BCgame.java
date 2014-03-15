a3cd4e9fd23ac807af9b8fdcd3b2ce1cba722be8
Erik <a>
2014-03-15 12:26:24 +0200

    yolo

---
+++ src/BCgame.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class BCgame {
+    public static void main(String[] args) {
+        int bulls = 0, cows = 0;
+        int cnum[] = new int[4];
+
+        Random r= new Random();
+        Scanner scan = new Scanner(System.in);
+
+        public void unique(){
+            System.out.println("Test");
+
+        };
+
+        do{
+            System.out.println("What is the number?: ");
+            String usernum=scan.nextLine();
+
+
+
+        }while(bulls<4);
+
+    }
+}
